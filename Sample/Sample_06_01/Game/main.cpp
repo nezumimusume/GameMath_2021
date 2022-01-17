061a4db6d119db45f2a4d98a10e89e7edbee749c
TakayukiKiyohara <takayukiKiyohara@gmail.com>
2022-01-17 23:32:45 +0900

    回転行列のテキストを加筆。

---
+++ Sample/Sample_06_01/Game/main.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include "system/system.h"
+
+
+///////////////////////////////////////////////////////////////////
+// ウィンドウプログラムのメイン関数。
+///////////////////////////////////////////////////////////////////
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+	//ゲームの初期化。
+	InitGame(hInstance, hPrevInstance, lpCmdLine, nCmdShow, TEXT("Game"));
+
+	
+	// ここからゲームループ。
+	while (DispatchWindowMessage())
+	{
+		K2Engine::GetInstance()->Execute();
+	
+	}
+	K2Engine::DeleteInstance();
+
+#ifdef _DEBUG
+	ReportLiveObjects();
+#endif // _DEBUG
+	return 0;
+}
+
