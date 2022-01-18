1a4f0b550ee9a1076d41eb303ab1ecc534602717
TakayukiKiyohara <TakayukiKiyohara@gmail.com>
2022-01-18 11:30:52 +0900

    6.1.5のハンズオン完成。

--- Sample/Sample_06_01/Game/main.cpp
+++ Sample/Sample_06_01/Game/main.cpp
@@ -1,6 +1,6 @@
 #include "stdafx.h"
 #include "system/system.h"
-
+#include "Background.h"
 
 ///////////////////////////////////////////////////////////////////
 // ウィンドウプログラムのメイン関数。
@@ -9,19 +9,32 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 {
 	//ゲームの初期化。
 	InitGame(hInstance, hPrevInstance, lpCmdLine, nCmdShow, TEXT("Game"));
+	Background* bg = NewGO< Background>(0);
 
+	g_camera3D->SetTarget(g_vec3Zero);
+	g_camera3D->SetPosition({ 0.0f, 0.0f, -200.0f });
 	
+	g_sceneLight->SetDirectionLight(1, { 0.0f, 0.0f, 1.0f }, { 1.2f, 1.2f, 1.2f });
+	
+	// step-1 ベクトルを定義する。
+
 	// ここからゲームループ。
 	while (DispatchWindowMessage())
 	{
+		// step-3 コントローラーの入力でZ軸周りの回転行列を作成して回転させる。
+		
+		// step-4 コントローラーの入力でX軸周りの回転行列を作成して回転させる。
+		
+		// step-5 コントローラーの入力でY軸周りの回転行列を作成して回転させる。
+
+		// step-2 確認のためにベクトルを表示する。
+
 		K2Engine::GetInstance()->Execute();
 	
 	}
+	// DeleteGO(bg);
 	K2Engine::DeleteInstance();
 
-#ifdef _DEBUG
-	ReportLiveObjects();
-#endif // _DEBUG
 	return 0;
 }
 
